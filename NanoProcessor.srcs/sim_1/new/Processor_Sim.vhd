8644fa276e8d8e6d9e9e8faea4d3b6e28d737c5c
Tharusha Dinujaya <tharushadinujayanew@gmail.com>
2023-05-30 15:01:37 +0530

    Create Simulation For Process

---
+++ NanoProcessor.srcs/sim_1/new/Processor_Sim.vhd
@@ -0,0 +1,67 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/30/2023 02:52:06 PM
+-- Design Name: 
+-- Module Name: Processor_Sim - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Processor_Sim is
+--  Port ( );
+end Processor_Sim;
+
+architecture Behavioral of Processor_Sim is
+Component Processor 
+    PORT(
+        Rst : in STD_LOGIC;
+        Clk : in STD_LOGIC;
+        Carry : out STD_LOGIC;
+        Zeroes : out STD_LOGIC;
+        R7_out : out STD_LOGIC_VECTOR (3 downto 0));
+End Component;
+
+    SIGNAL Rst, Clk, Carry, Zeroes : STD_LOGIC;
+    SIGNAL R7_out : STD_LOGIC_VECTOR(3 downto 0);
+begin
+    UUT : Processor
+        PORT MAP(
+            Rst => Rst,
+            Clk => Clk,
+            Carry => Carry,
+            Zeroes => Zeroes,
+            R7_out => R7_out);
+
+    process 
+        begin
+            Clk <= '0';
+            wait for 10ns;
+            
+            Clk <= '1';
+            wait for 10ns;
+    end process;
+end Behavioral;
