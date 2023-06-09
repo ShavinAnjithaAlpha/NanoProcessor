4f1ba6c0524f23daaaf97d9b8e3c290120d95eb2
Tharusha Dinujaya <tharushadinujayanew@gmail.com>
2023-06-09 16:34:50 +0530

    Update Simulations of multiplicator

--- NanoProcessor.srcs/sim_1/new/Adder_8Bit_Sim.vhd
+++ NanoProcessor.srcs/sim_1/new/Adder_8Bit_Sim.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 06/04/2023 12:33:48 PM
+-- Create Date: 06/09/2023 04:32:05 PM
 -- Design Name: 
--- Module Name: Adder_4Bit_Sim - Behavioral
+-- Module Name: Adder_8Bit_Sim - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -31,24 +31,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity Adder_4Bit_Sim is
+entity Adder_8Bit_Sim is
 --  Port ( );
-end Adder_4Bit_Sim;
+end Adder_8Bit_Sim;
 
-architecture Behavioral of Adder_4Bit_Sim is
-Component Adder_4Bit
-    PORT(   A_in : in STD_LOGIC_VECTOR (3 downto 0);
+architecture Behavioral of Adder_8Bit_Sim is
+Component Adder_8Bit
+    PORT(   A_in : in STD_LOGIC_VECTOR (7 downto 0);
             B_in : in STD_LOGIC;
-            S_out : out STD_LOGIC_VECTOR (3 downto 0);
-            Q : in STD_LOGIC_VECTOR (3 downto 0);
+            S_out : out STD_LOGIC_VECTOR (7 downto 0);
+            Q : in STD_LOGIC_VECTOR (7 downto 0);
             LSB : out STD_LOGIC);
 End Component;
 
-SIGNAL A_in, Q, S_out : STD_LOGIC_VECTOR(3 downto 0);
+SIGNAL A_in, Q, S_out : STD_LOGIC_VECTOR(7 downto 0);
 SIGNAL LSB, B_in : STD_LOGIC;
 
 begin
-    UUT : Adder_4Bit
+    UUT : Adder_8Bit
         PORT MAP(
             A_in => A_in,
             B_in => B_in,
@@ -58,31 +58,30 @@ begin
         
     process
     begin
-        A_in <= "0000";
+        A_in <= "00000000";
         B_in <= '1';
-        Q <= "1111";
+        Q <= "11111111";
         wait for 10ns;
         
-        A_in <= "0001";
+        A_in <= "00000001";
         B_in <= '1';
-        Q <= "1111";
+        Q <= "11111111";
         wait for 10ns;
         
-        A_in <= "0010";
+        A_in <= "00000010";
         B_in <= '1';
-        Q <= "1111";
+        Q <= "11111111";
         wait for 10ns;
         
-        A_in <= "0100";
+        A_in <= "00000100";
         B_in <= '1';
-        Q <= "1111";
+        Q <= "11111111";
         wait for 10ns;
                 
-        A_in <= "1000";
+        A_in <= "00001000";
         B_in <= '1';
-        Q <= "1111";
+        Q <= "11111111";
         wait;
     end process;
 
 end Behavioral;
-
