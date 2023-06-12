3d53370dc0bc5493b2508cbdace6f32d17c27c02
J.P. Shavin Anjitha Chandrawansha <shavinanjithachandrawansha@gmail.com>
2023-06-12 11:33:56 +0530

    fix the ussue with the 7-seg display pin mapping

--- NanoProcessor.srcs/sources_1/new/LUT_7_Display.vhd
+++ NanoProcessor.srcs/sources_1/new/LUT_7_Display.vhd
@@ -87,22 +87,22 @@ begin
     process(LED_BCD)
     begin
         case LED_BCD is
-        when "0000" => LED_out <= "0000001"; -- "0"     
-        when "0001" => LED_out <= "1001111"; -- "1" 
-        when "0010" => LED_out <= "0010010"; -- "2" 
-        when "0011" => LED_out <= "0000110"; -- "3" 
-        when "0100" => LED_out <= "1001100"; -- "4" 
-        when "0101" => LED_out <= "0100100"; -- "5" 
-        when "0110" => LED_out <= "0100000"; -- "6" 
-        when "0111" => LED_out <= "0001111"; -- "7" 
+        when "0000" => LED_out <= "1000000"; -- "0"     
+        when "0001" => LED_out <= "1111001"; -- "1" 
+        when "0010" => LED_out <= "0100100"; -- "2" 
+        when "0011" => LED_out <= "0110000"; -- "3" 
+        when "0100" => LED_out <= "0011001"; -- "4" 
+        when "0101" => LED_out <= "0010010"; -- "5" 
+        when "0110" => LED_out <= "0000010"; -- "6" 
+        when "0111" => LED_out <= "1111000"; -- "7" 
         when "1000" => LED_out <= "0000000"; -- "8"     
-        when "1001" => LED_out <= "0000100"; -- "9" 
-        when "1010" => LED_out <= "0000010"; -- a
-        when "1011" => LED_out <= "1100000"; -- b
-        when "1100" => LED_out <= "0110001"; -- C
-        when "1101" => LED_out <= "1000010"; -- d
-        when "1110" => LED_out <= "0110000"; -- E
-        when "1111" => LED_out <= "0111000"; -- F
+        when "1001" => LED_out <= "0010000"; -- "9" 
+        when "1010" => LED_out <= "0001000"; -- a
+        when "1011" => LED_out <= "0000011"; -- b
+        when "1100" => LED_out <= "1000110"; -- C
+        when "1101" => LED_out <= "0100001"; -- d
+        when "1110" => LED_out <= "0000110"; -- E
+        when "1111" => LED_out <= "0001110"; -- F
         when others => LED_out <= "0000000";
         end case;
     end process;
